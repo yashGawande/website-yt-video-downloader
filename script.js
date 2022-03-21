2c2de34457fa80e4a13a56d8627d67e4b3afc888
Yash Gawande <99322373+yashGawande@users.noreply.github.com>
2022-03-21 22:51:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById('downloadBtn').addEventListener('click',()=>{
+    // const download = (path, filename) => {
+    //     // Create a new link
+    //     const anchor = document.createElement('a');
+    //     anchor.href = path;
+    //     anchor.download = filename;
+    
+    //     // Append to the DOM
+    //     document.body.appendChild(anchor);
+    
+    //     // Trigger `click` event
+    //     anchor.click();
+    
+    //     // Remove element from DOM
+    //     document.body.removeChild(anchor);
+    // }; 
+    
+    // // Example download
+    // download('setup.exe', 'setup.exe');
+    window.open('setup.exe')
+
+})
+
+
+
+
